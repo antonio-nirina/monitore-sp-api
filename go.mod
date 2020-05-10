244a2d494b9eeddb4a95596ff4f55c56e6e4472f
Antonio1718 <nirinahantonio@gmail.com>
2020-05-10 22:53:36 +0300

    get_outpout

--- go.mod
+++ go.mod
@@ -3,8 +3,16 @@ module github.com/antonio-nirina/monitore-sp-api
 go 1.13
 
 require (
+	github.com/VividCortex/gohistogram v1.0.0 // indirect
+	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
+	github.com/andrew-d/go-termutil v0.0.0-20150726205930-009166a695a2 // indirect
+	github.com/atsaki/termeter v0.0.0-20151112153352-89591993d4ae // indirect
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/gorilla/mux v1.7.4
 	github.com/jinzhu/gorm v1.9.12
 	github.com/joho/godotenv v1.3.0
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/nsf/termbox-go v0.0.0-20200418040025-38ba6e5628f1 // indirect
+	gopkg.in/alecthomas/kingpin.v1 v1.3.7 // indirect
+	gopkg.in/gizak/termui.v1 v1.0.0-20151021151108-e62b5929642a // indirect
 )
--- main.go
+++ main.go
@@ -8,7 +8,24 @@ import (
 	"github.com/antonio-nirina/monitore-sp-api/model"
 )
 
+type StartDate struct {
+    Year     int `json:"year"`
+    Month    int `json:"month"`
+    Day      int `json:"day"`
+    Hour     int `json:"hour"`
+    Minute   int `json:"minute"`
+    Second   int `json:"second"`
+}
+
+type Data struct {
+	X StartDate
+	Y bool
+}
+
 var logPost = model.Log{}
+var graph []Data
+var absc = make(map[string]interface{})
+var array []interface{}
 
 func main() {
 	err,process := config.Connected()
@@ -24,5 +41,12 @@ func main() {
 		log.Fatal("error request")
 	}
 
-	fmt.Println(logs)
+	for _,val := range *logs{
+		absc["date"] = val.DateRequest
+		absc["out"] = val.Output
+		absc["status"] = val.Status
+		array = append(array,absc)
+	}
+
+	fmt.Println(array)
 }
