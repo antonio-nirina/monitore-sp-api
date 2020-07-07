909d3bd2d14a3bc5cff2e06b1044f4c2eb0c2565
Antonio1718 <nirinahantonio@gmail.com>
2020-07-08 00:48:48 +0300

    change example

--- example/democli.go
+++ example/democli.go
@@ -5,203 +5,140 @@
 package example
 
 import (
+	"errors"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
 	"strings"
 
 	"github.com/jroimartin/gocui"
 )
 
-func nextView(g *gocui.Gui, v *gocui.View) error {
-	if v == nil || v.Name() == "side" {
-		_, err := g.SetCurrentView("main")
-		return err
-	}
-	_, err := g.SetCurrentView("side")
-	return err
+const delta = 0.2
+
+type HelpWidget struct {
+	name string
+	x, y int
+	w, h int
+	body string
 }
 
-func cursorDown(g *gocui.Gui, v *gocui.View) error {
-	if v != nil {
-		cx, cy := v.Cursor()
-		if err := v.SetCursor(cx, cy+1); err != nil {
-			ox, oy := v.Origin()
-			if err := v.SetOrigin(ox, oy+1); err != nil {
-				return err
-			}
+func NewHelpWidget(name string, x, y int, body string) *HelpWidget {
+	lines := strings.Split(body, "\n")
+
+	w := 0
+	for _, l := range lines {
+		if len(l) > w {
+			w = len(l)
 		}
 	}
-	return nil
+	h := len(lines) + 1
+	w = w + 1
+
+	return &HelpWidget{name: name, x: x, y: y, w: w, h: h, body: body}
 }
 
-func cursorUp(g *gocui.Gui, v *gocui.View) error {
-	if v != nil {
-		ox, oy := v.Origin()
-		cx, cy := v.Cursor()
-		if err := v.SetCursor(cx, cy-1); err != nil && oy > 0 {
-			if err := v.SetOrigin(ox, oy-1); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-func getLine(g *gocui.Gui, v *gocui.View) error {
-	var l string
-	var err error
-
-	_, cy := v.Cursor()
-	if l, err = v.Line(cy); err != nil {
-		l = ""
-	}
-
-	maxX, maxY := g.Size()
-	if v, err := g.SetView("msg", maxX/2-30, maxY/2, maxX/2+30, maxY/2+2); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		fmt.Fprintln(v, l)
-		if _, err := g.SetCurrentView("msg"); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func delMsg(g *gocui.Gui, v *gocui.View) error {
-	if err := g.DeleteView("msg"); err != nil {
-		return err
-	}
-	if _, err := g.SetCurrentView("side"); err != nil {
-		return err
-	}
-	return nil
-}
-
-func quit(g *gocui.Gui, v *gocui.View) error {
-	return gocui.ErrQuit
-}
-
-func keybindings(g *gocui.Gui) error {
-	if err := g.SetKeybinding("side", gocui.KeyCtrlSpace, gocui.ModNone, nextView); err != nil {
-		return err
-	}
-	if err := g.SetKeybinding("main", gocui.KeyCtrlSpace, gocui.ModNone, nextView); err != nil {
-		return err
-	}
-	if err := g.SetKeybinding("side", gocui.KeyArrowDown, gocui.ModNone, cursorDown); err != nil {
-		return err
-	}
-	if err := g.SetKeybinding("side", gocui.KeyArrowUp, gocui.ModNone, cursorUp); err != nil {
-		return err
-	}
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		return err
-	}
-	if err := g.SetKeybinding("side", gocui.KeyEnter, gocui.ModNone, getLine); err != nil {
-		return err
-	}
-	if err := g.SetKeybinding("msg", gocui.KeyEnter, gocui.ModNone, delMsg); err != nil {
-		return err
-	}
-
-	if err := g.SetKeybinding("main", gocui.KeyCtrlS, gocui.ModNone, saveMain); err != nil {
-		return err
-	}
-	if err := g.SetKeybinding("main", gocui.KeyCtrlW, gocui.ModNone, saveVisualMain); err != nil {
-		return err
-	}
-	return nil
-}
-
-func saveMain(g *gocui.Gui, v *gocui.View) error {
-	f, err := ioutil.TempFile("", "gocui_demo_")
+func (w *HelpWidget) Layout2(g *gocui.Gui) error {
+	v, err := g.SetView(w.name, w.x, w.y, w.x+w.w, w.y+w.h)
 	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	p := make([]byte, 5)
-	v.Rewind()
-	for {
-		n, err := v.Read(p)
-		if n > 0 {
-			if _, err := f.Write(p[:n]); err != nil {
-				return err
-			}
-		}
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
+		if err != gocui.ErrUnknownView {
 			return err
 		}
+		fmt.Fprint(v, w.body)
 	}
 	return nil
 }
 
-func saveVisualMain(g *gocui.Gui, v *gocui.View) error {
-	f, err := ioutil.TempFile("", "gocui_demo_")
+type StatusbarWidget struct {
+	name string
+	x, y int
+	w    int
+	val  float64
+}
+
+func NewStatusbarWidget(name string, x, y, w int) *StatusbarWidget {
+	return &StatusbarWidget{name: name, x: x, y: y, w: w}
+}
+
+func (w *StatusbarWidget) SetVal(val float64) error {
+	if val < 0 || val > 1 {
+		return errors.New("invalid value")
+	}
+	w.val = val
+	return nil
+}
+
+func (w *StatusbarWidget) Val() float64 {
+	return w.val
+}
+
+func (w *StatusbarWidget) Layout(g *gocui.Gui) error {
+	v, err := g.SetView(w.name, w.x, w.y, w.x+w.w, w.y+2)
+	if err != nil && err != gocui.ErrUnknownView {
+		return err
+	}
+	v.Clear()
+
+	rep := int(w.val * float64(w.w-1))
+	fmt.Fprint(v, strings.Repeat("▒", rep))
+	return nil
+}
+
+type ButtonWidget struct {
+	name    string
+	x, y    int
+	w       int
+	label   string
+	handler func(g *gocui.Gui, v *gocui.View) error
+}
+
+func NewButtonWidget(name string, x, y int, label string, handler func(g *gocui.Gui, v *gocui.View) error) *ButtonWidget {
+	return &ButtonWidget{
+		name: name, 
+		x: x,
+		y: y, 
+		w: len(label) + 1, 
+		label: label, 
+		handler: handler
+	}
+}
+
+func (w *ButtonWidget) Layout11a(g *gocui.Gui) error {
+	v, err := g.SetView(w.name, w.x, w.y, w.x+w.w, w.y+2)
 	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	vb := v.ViewBuffer()
-	if _, err := io.Copy(f, strings.NewReader(vb)); err != nil {
-		return err
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		if _, err := g.SetCurrentView(w.name); err != nil {
+			return err
+		}
+		if err := g.SetKeybinding(w.name, gocui.KeyEnter, gocui.ModNone, w.handler); err != nil {
+			return err
+		}
+		fmt.Fprint(v, w.label)
 	}
 	return nil
 }
 
-func layout(g *gocui.Gui) error {
-	maxX, maxY := g.Size()
-	if v, err := g.SetView("side", -1, -1, 30, maxY); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		v.Highlight = true
-		v.SelBgColor = gocui.ColorGreen
-		v.SelFgColor = gocui.ColorBlack
-		fmt.Fprintln(v, "Item 1")
-		fmt.Fprintln(v, "Item 2")
-		fmt.Fprintln(v, "Item 3")
-		fmt.Fprint(v, "\rWill be")
-		fmt.Fprint(v, "deleted\rItem 4\nItem 5")
-	}
-	if v, err := g.SetView("main", 30, -1, maxX, maxY); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		b, err := ioutil.ReadFile("Mark.Twain-Tom.Sawyer.txt")
-		if err != nil {
-			panic(err)
-		}
-		fmt.Fprintf(v, "%s", b)
-		v.Editable = true
-		v.Wrap = true
-		if _, err := g.SetCurrentView("main"); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func main() {
+func mainazz() {
 	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
 		log.Panicln(err)
 	}
 	defer g.Close()
 
-	g.Cursor = true
+	g.Highlight = true
+	g.SelFgColor = gocui.ColorRed
 
-	g.SetManagerFunc(layout)
+	help := NewHelpWidget("help", 1, 1, helpText)
+	status := NewStatusbarWidget("status", 1, 7, 50)
+	butdown := NewButtonWidget("butdown", 52, 7, "DOWN", statusDown(status))
+	butup := NewButtonWidget("butup", 58, 7, "UP", statusUp(status))
+	g.SetManager(help, status, butdown, butup)
 
-	if err := keybindings(g); err != nil {
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+	if err := g.SetKeybinding("", gocui.KeyTab, gocui.ModNone, toggleButton); err != nil {
 		log.Panicln(err)
 	}
 
@@ -209,3 +146,41 @@ func main() {
 		log.Panicln(err)
 	}
 }
+
+func quit1(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
+
+func toggleButton(g *gocui.Gui, v *gocui.View) error {
+	nextview := "butdown"
+	if v != nil && v.Name() == "butdown" {
+		nextview = "butup"
+	}
+	_, err := g.SetCurrentView(nextview)
+	return err
+}
+
+func statusUp(status *StatusbarWidget) func(g *gocui.Gui, v *gocui.View) error {
+	return func(g *gocui.Gui, v *gocui.View) error {
+		return statusSet(status, delta)
+	}
+}
+
+func statusDown(status *StatusbarWidget) func(g *gocui.Gui, v *gocui.View) error {
+	return func(g *gocui.Gui, v *gocui.View) error {
+		return statusSet(status, -delta)
+	}
+}
+
+func statusSet(sw *StatusbarWidget, inc float64) error {
+	val := sw.Val() + inc
+	if val < 0 || val > 1 {
+		return nil
+	}
+	return sw.SetVal(val)
+}
+
+const helpText = `KEYBINDINGS
+Tab: Move between buttons
+Enter: Push button
+^C: Exit`
--- xcli.go
+++ xcli.go
@@ -1,121 +1,48 @@
 package main
-
 import (
 	"fmt"
 	"log"
+	//"math"
 	"strings"
 
 	"github.com/jroimartin/gocui"
 )
-
-const noteText = `KEYBINDINGS
+const noteText = `
 Monotoring Log Sp-Api
 CHECK STATUS SERVICE
-^C: Exit
+^C = Exit
 `
 
-type xWidget struct {
-	name string
-	x, y int
-	w, h int
-	body string
+type result struct {
+	st []string
+	view *gocui.Gui
 }
 
-type xDataList struct {
-	name string
-	x, y int
-	w, h int
-	body string
+type helpStatus struct {
+	x int
+	y int 
 }
-
-var xd xDataList
-var xW xWidget
-
-func newNotice(title string, x, y int, body string) *xWidget {
-	lines := strings.Split(body, "\n")
-	w := 0
-	for _, l := range lines {
-		if len(l) > w {
-			w = len(l)
-		}
-	}
-	h := len(lines) + 1
-	w = w + 1
-
-	xW.name = title
-	xW.x = x
-	xW.h = h
-	xW.w = w
-	xW.body = body
-	xW.y = y
-
-	return &xW
-}
-
-func listDataLog(title string, x, y int, body string) *xDataList {
-	xd.body = body
-	xd.name = title
-	xd.h = 10
-	xd.x = x
-	xd.y = y
-	xd.w = 100
-
-	return &xd
-}
-func downKey(g *gocui.Gui, v *gocui.View) error {
-	if v != nil {
-		cx, cy := v.Cursor()
-		if err := v.SetCursor(cx, cy+1); err != nil {
-			ox, oy := v.Origin()
-			if err := v.SetOrigin(ox, oy+1); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-func upKey(g *gocui.Gui, v *gocui.View) error {
-	if v != nil {
-		ox, oy := v.Origin()
-		cx, cy := v.Cursor()
-		if err := v.SetCursor(cx, cy-1); err != nil && oy > 0 {
-			if err := v.SetOrigin(ox, oy-1); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-func keypressing(g *gocui.Gui) error {
-	if err := g.SetKeybinding("side", gocui.KeyArrowDown, gocui.ModNone, downKey); err != nil {
-		return err
-	}
-	if err := g.SetKeybinding("side", gocui.KeyArrowUp, gocui.ModNone, upKey); err != nil {
-		return err
-	}
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		return err
-	}
-
-	return nil
-}
-
+var res result
+var hl helpStatus
 func main() {
 	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
 		log.Panicln(err)
 	}
 	defer g.Close()
-	g.Cursor = true
 
-	notice := newNotice("Monitore Sp-api", 1, 0, noteText)
-	layout := listDataLog("Logrus", 30, 0, "List Api")
-	g.SetManager(notice, layout)
+	g.SetManagerFunc(layout)
 
-	if err := keypressing(g); err != nil {
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
 		log.Panicln(err)
+	}
+
+	if err := g.SetKeybinding("", gocui.KeyArrowDown, gocui.ModNone, setListStatus(1)); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.SetKeybinding("", gocui.KeyArrowDown, gocui.ModNone, setListStatus(-1)); err != nil {
+			log.Panicln(err)
 	}
 
 	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
@@ -123,45 +50,76 @@ func main() {
 	}
 }
 
-func (w *xWidget) Layout(g *gocui.Gui) error {
-	//_, maxY := g.Size()
-	wdt := w.x + w.w
-	v, err := g.SetView(w.name, w.x, w.y, w.x+w.w, w.y+w.h)
-	if err != nil {
+func layout(g *gocui.Gui) error {
+	// maxX, maxY := g.Size()
+	w,h := sizeTitle() 
+	list := []string{"Item1","Item2","Item3","Item4","Item5"}
+	res.st = list
+	hl.x = 1
+	hl.y = 0
+	if v, err := g.SetView("Monitore Sp-api", 1, 0, w, h); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		fmt.Fprint(v, w.body)
+		fmt.Fprintln(v, noteText)
 	}
-
-	if v, err := g.SetView("side", 1, 8, wdt, 20); err != nil {
+	if v, err := g.SetView("st1", 1, 6, w, 7+len(list)); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		v.Title = "Error"
+
+		v.Title = "Status"
 		v.Highlight = true
-		v.SelBgColor = gocui.ColorGreen
-		v.SelFgColor = gocui.ColorRed
-		fmt.Fprintln(v, "Item 1")
-		fmt.Fprintln(v, "Item 2")
-		fmt.Fprintln(v, "Item 3")
-		fmt.Fprint(v, "deleted\rItem 4\nItem 5")
+		setState(v)
+	}
+	if v, err := g.SetView("Log", w+2, 0, 50, 10); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Title = "Log"
+		fmt.Fprintln(v, "Data")
 	}
 	return nil
 }
 
-func (d *xDataList) Layout(g *gocui.Gui) error {
-	v, err := g.SetView(d.name, d.x, d.y, d.x+d.w, d.y+d.h)
-	if err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
+func sizeTitle() (int,int) {
+	lines := strings.Split(noteText, "\n")
+	w := 0
+	for _, l := range lines {
+		if len(l) > w {
+			w = len(l)
 		}
-		fmt.Fprint(v, d.body)
 	}
-	v.Title = "Details"
-	return nil
+	h := len(lines) 
+	w = w + 2
+
+	return w,h
 }
+
+func setListStatus(d int) func(g *gocui.Gui, v *gocui.View) error {
+	return func(g *gocui.Gui, v *gocui.View) error {
+		if hl.y < len(res.st) && d == 1 {
+			hl.y++
+		}
+		if hl.y > len(res.st) && d == -1{
+			hl.y--
+		}
+		setState(v)
+		return nil
+	}
+}
+
+func setState(vs *gocui.View) {
+	vs.SelBgColor = gocui.ColorGreen
+	vs.SelFgColor = gocui.ColorRed
+	vs.SetCursor(hl.x,hl.y)
+	for _,val := range res.st {
+		fmt.Fprintln(vs, val)
+	}
+}
+
 
 func quit(g *gocui.Gui, v *gocui.View) error {
 	return gocui.ErrQuit
 }
+
